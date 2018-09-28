42f7c86b07e69b880ee4c5601b771041f84bac8c
王楠魁 <wnk@e-trans.com.cn>
2018-09-28 17:33:19 +0800

    1.同上

---
+++ app/src/main/java/com/etrans/bluetooth/le/fragment/FragmentThree.java
@@ -0,0 +1,25 @@
+package com.etrans.bluetooth.le.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.etrans.bluetooth.le.R;
+
+/**
+ * 项目名称：FragmentTrasaction
+ * 创建人：Double2号
+ * 创建时间：2017.3.3 10:35
+ * 修改备注：
+ */
+public class FragmentThree extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_three,container,false);
+        return view;
+    }
+}
